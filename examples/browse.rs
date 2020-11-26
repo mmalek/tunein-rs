cf17218aec340164ca82e075d689ffd5c408504a
Michal Malek <michalm@fastmail.fm>
2020-11-26 17:24:48 +0100

    Show formatted output in browse example

--- examples/browse.rs
+++ examples/browse.rs
@@ -1,13 +1,16 @@
 use std::error::Error;
 use std::io::Cursor;
 use tokio::stream::StreamExt;
+use tunein::Outline;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    println!("Browsing...");
+    let uri = std::env::args()
+        .nth(1)
+        .unwrap_or_else(|| tunein::request::BROWSE_URI.to_string());
 
     let client = hyper::Client::new();
-    let response = client.get(tunein::request::BROWSE_URI.parse()?).await?;
+    let response = client.get(uri.parse()?).await?;
     let mut body = response.into_body();
 
     let mut buffer = Vec::new();
@@ -15,7 +18,25 @@ async fn main() -> Result<(), Box<dyn Error>> {
         buffer.extend_from_slice(&bytes?);
     }
 
-    println!("{:#?}", tunein::read(Cursor::new(buffer))?);
+    let document = tunein::read(Cursor::new(buffer))?;
+
+    println!("{}\n-----", document.head.title);
+
+    print_outlines(&document.outlines, 0);
 
     Ok(())
+}
+
+fn print_outlines(outlines: &[Outline], depth: usize) {
+    for outline in outlines {
+        print!("{:>indent$}", "", indent = 4 * depth);
+        match outline {
+            Outline::Group(group) => {
+                println!("{}:", group.text);
+                print_outlines(&group.outlines, depth + 1);
+            }
+            Outline::Link(link) => println!("{} - {}", link.text, link.url),
+            Outline::Audio(audio) => println!("{} - {}", audio.text, audio.url),
+        }
+    }
 }
